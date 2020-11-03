e142fd25e65c96c0605603fd8182c74554d3a791
rashmiahuj <rashmi.ahuja2088@gmail.com>
2020-11-03 11:58:50 +0530

    Update rash.py

--- rash.py
+++ rash.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def hello(): 
 	return "welcome to the Oracle web app with python in DevSecOPs !! This is new change"
 
+@app.route('/oracle') 
+def hello(): 
+	return "Welcome to Oracle Containerization process !!"
+
 
 if __name__ == "__main__": 
 	app.run(host ='0.0.0.0', port = 5000, debug = True) 
